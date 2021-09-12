99af0e059d5b3cd39210517c8b618bba05ef03d6
Quixotic95 <e.oguz95@hotmail.com>
2021-09-12 13:39:19 +0300

    controls added

--- patika-06-01/src/main/java/dev/patika/patika0601/service/EmployeeService.java
+++ patika-06-01/src/main/java/dev/patika/patika0601/service/EmployeeService.java
@@ -5,6 +5,7 @@ import dev.patika.patika0601.repository.EmployeeRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @Service
@@ -12,17 +13,19 @@ import java.util.List;
 public class EmployeeService {
     private final EmployeeRepository repository;
 
-    public List<Employee> getAllEmployees(){
+    public List<Employee> getAllEmployees() {
         return repository.findAll();
     }
 
     public void addEmployee(Employee employee) {
         // TO-DO check if employee exists???
+        repository.findEmployeeByEmail(employee.getEmail()).orElseThrow(() -> new RuntimeException("An Employee with this e-mail already exists."));
         repository.save(employee);
     }
 
     public void deleteEmployee(Long employeeId) {
         // TO-DO check if employee exists with given ID???
+        repository.findById(employeeId).orElseThrow(() -> new EntityNotFoundException("No Employee found on database with id: " + employeeId));
         repository.deleteById(employeeId);
 
     }
